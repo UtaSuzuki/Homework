112f97b4ad92c347951fc03981894194718a6d1a
YutaSuzuki <szyt0x9a@yahoo.co.jp>
2020-10-24 06:52:33 +0000

    Rails04Homework-correct1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
     validates :name, presence: true, length: {maximum: 15}
-    validates :email, format: { with: /\A[a-zA-Z]\w*@\w+[.]\w+\z/}
-    validates :password, # length: {minimum: 8, maximum: 32}, 
-        format: { with: /\A[a-zA-Z](?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{7,31}\z/ }
+    validates :email, format: { with: /\A[\w\-\.]+@[a-zA-Z\d\-]+\.[a-zA-Z]+\z/}
+    validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{8,32}\z/ }
 
     has_secure_password
 end
